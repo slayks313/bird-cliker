4f497394005eb2ed1ddc8cfa7d0d1a5190f43a82
slayks313 <slayks147@gmail.com>
2024-08-16 11:30:24 +0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,64 @@
+const $circle = document.querySelector('#circle');
+const $score = document.querySelector('#score');
+
+function start() {
+    setScore(getScore());
+    setImage()
+}
+
+function setScore(score) {
+    localStorage.setItem('score', score);
+    $score.textContent = score;
+}
+
+function getScore() {
+    return Number(localStorage.getItem('score')) || 0; 
+}
+
+function addOne() {
+    setScore(getScore() + 1);
+    setImage()
+}
+function setImage(){
+    if(getScore() >= 500) {
+      $circle.setAttribute('src','./assets/bird2.png')
+    }
+}
+
+$circle.addEventListener('click', (event) => {
+    const rect = $circle.getBoundingClientRect();
+    
+    const offsetX = event.clientX - rect.left - rect.width / 2;
+    const offsetY = event.clientY - rect.top - rect.height / 2;
+    
+    const DEG = 40;
+    
+    const tiltX = (offsetY / rect.height) * DEG;
+    const tiltY = (offsetX / rect.width) * -DEG;
+    
+    $circle.style.setProperty('--tiltX', `${tiltX}deg`);
+    $circle.style.setProperty('--tiltY', `${tiltY}deg`);
+    
+    setTimeout(() => {
+        $circle.style.setProperty('--tiltX', `0deg`);
+        $circle.style.setProperty('--tiltY', `0deg`);
+    }, 300);
+
+    const plusOne = document.createElement('div');
+    plusOne.classList.add('plus-one');
+    plusOne.textContent = '+1';
+    plusOne.style.position = 'absolute';
+    plusOne.style.left = `${event.clientX - rect.left}px`;
+    plusOne.style.top = `${event.clientY - rect.top}px`;
+
+    $circle.parentElement.appendChild(plusOne);
+
+    addOne();
+
+    setTimeout(() => { 
+        plusOne.remove();
+    }, 2000);
+});
+
+
+start();
